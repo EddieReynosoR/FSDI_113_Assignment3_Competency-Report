be5c05c6d687c21cfaf405419d22b4bcd2dbfb08
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2022-09-29 20:20:44 -0700

    FSDI 113 Code Classes

---
+++ heroku_config.sh
@@ -0,0 +1,6 @@
+
+heroku config: EMAIL_BACKEND=console
+heroku config: EMAIL_HOST = smtp.sendgrid.net
+heroku config: EMAIL_HOST_USER= apikey
+heroku config: EMAIL_HOST_PASSWORD = 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
+heroku config: DEFAULT_FROM_EMAIL=eduardo_3reynoso@hotmail.com
